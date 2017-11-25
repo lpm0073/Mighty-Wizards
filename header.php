99c34ec38eb6550e40d52c0719a454c9e92035e5
Lawrence McDaniel <lpm0073@gmail.com>
2017-11-25 13:46:08 -0600

    cleanup head

--- header.php
+++ header.php
@@ -27,12 +27,9 @@
 		<!-- End: Imported from legacy Mighty Wizards site -->
 
 
-		<link href="//www.google-analytics.com" rel="dns-prefetch">
-    <link href="<?php echo get_template_directory_uri(); ?>/img/favicon.png" rel="shortcut icon">
-    <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
-
 		<!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="//www.google-analytics.com" rel="dns-prefetch">
 
 		<?php wp_head(); ?>
 
