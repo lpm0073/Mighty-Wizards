997a6b669349c710a65bebb97b79c52bdedaadec
Lawrence McDaniel <lpm0073@gmail.com>
2017-11-25 09:27:24 -0600

    create install script for wordpress theme

--- install.sh
+++ install.sh
@@ -3,5 +3,11 @@
 cd ~/
 git clone https://github.com/lpm0073/Mighty-Wizards.git
 
+sudo chown -R apache /home/ec2-user/Mighty-Wizards
+sudo chgrp -R apache /home/ec2-user/Mighty-Wizards
+sudo chmod 2775 /home/ec2-user/Mighty-Wizards
+find /home/ec2-user/Mighty-Wizards -type d -exec sudo chmod 2775 {} \;
+find /home/ec2-user/Mighty-Wizards -type f -exec sudo chmod 0664 {} \;
+
 sudo rm -r /var/www/html/wizards/wp-content/themes/Mighty-Wizards/
 sudo mv Mighty-Wizards /var/www/html/wizards/wp-content/themes/
