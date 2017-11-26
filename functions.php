bf30c2b8631a000df173c6a4a82f298e0a0ef1ab
Lawrence McDaniel <lpm0073@gmail.com>
2017-11-26 06:01:44 -0600

    add wp admin bar

--- functions.php
+++ functions.php
@@ -253,10 +253,10 @@ function html5_blank_view_article($more)
 }
 
 // Remove Admin bar
-function remove_admin_bar()
-{
-    return false;
-}
+//function remove_admin_bar()
+//{
+//    return false;
+//}
 
 // Remove 'text/css' from our enqueued stylesheet
 function html5_style_remove($tag)
@@ -374,7 +374,7 @@ add_filter('the_category', 'remove_category_rel_from_category_list'); // Remove 
 add_filter('the_excerpt', 'shortcode_unautop'); // Remove auto <p> tags in Excerpt (Manual Excerpts only)
 add_filter('the_excerpt', 'do_shortcode'); // Allows Shortcodes to be executed in Excerpt (Manual Excerpts only)
 add_filter('excerpt_more', 'html5_blank_view_article'); // Add 'View Article' button instead of [...] for Excerpts
-add_filter('show_admin_bar', 'remove_admin_bar'); // Remove Admin bar
+//add_filter('show_admin_bar', 'remove_admin_bar'); // Remove Admin bar
 add_filter('style_loader_tag', 'html5_style_remove'); // Remove 'text/css' from enqueued stylesheet
 add_filter('post_thumbnail_html', 'remove_thumbnail_dimensions', 10); // Remove width and height dynamic attributes to thumbnails
 add_filter('image_send_to_editor', 'remove_thumbnail_dimensions', 10); // Remove width and height dynamic attributes to post images
