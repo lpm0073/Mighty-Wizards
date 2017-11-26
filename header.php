6c8861c0280e164805c3bf724e1370bf11365e93
Lawrence McDaniel <lpm0073@gmail.com>
2017-11-26 08:18:58 -0600

    head tuning

--- header.php
+++ header.php
@@ -1,20 +1,28 @@
+/*
+ *  Author: Todd Motto | @toddmotto
+ *  URL: html5blank.com | @html5blank
+ *  Custom functions, support, custom post types and more.
+ */
+
 <!doctype html>
 <html <?php language_attributes(); ?> class="no-js">
+  <!----------------------------------------------------------------------------
+	Custom Wordpress Sites and Hosting
+	Lawrence McDaniel
+	https://lawrencemcdaniel.com
+	lpm0073@gmail.com
+	---------------------------------------------------------------------------->
 	<head>
-		<meta charset="<?php bloginfo('charset'); ?>">
 		<title>Mighty Wizards Technologies | Your nearshore BI Partner</title>
-
-		<!-- Begin: Imported from legacy Mighty Wizards site -->
+		<meta charset="<?php bloginfo('charset'); ?>">
 		<meta name="description" content="We offer top-notch end-to-end solutions for your unique BI implementation in your time zone">
+		<meta name="keywords" content="BI, Nearshore, Business Intelligence, Professional Services, Mexico">
+		<meta name="author" content="Mighty Wizards">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
 
-		<meta name="keywords" content="">
-		<meta charset="utf-8">
-		<meta name="author" content="">
-
-		<!-- Favicons -->
-		<link rel="shortcut icon" href="/wp-content/uploads/favicon.png">
-
-		<!-- CSS -->
+		<link rel="icon" type="image/png" href="/wp-content/uploads/favicon.png">
+		<link rel="dns-prefetch" href="//www.google-analytics.com">
 		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.min.css">
 		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
 		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style-responsive.css">
@@ -24,40 +32,25 @@
 		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/magnific-popup.css">
 		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/rev-slider.css">
 		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/rs-plugin/css/settings.css" media="screen" />
-		<!-- End: Imported from legacy Mighty Wizards site -->
-
-
-		<!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link href="//www.google-analytics.com" rel="dns-prefetch">
 
 		<?php wp_head(); ?>
 
 	</head>
-
-
-	<!-- Begin Legacy Mighty Wizards site -->
 	<body class="appear-animate">
-
-			<div class="page-loader">
-					<div class="loader">Loading...</div>
-			</div>
-
-			<div class="page" id="top">
-
-					<nav class="main-nav dark transparent stick-fixed">
-							<div class="full-wrapper relative clearfix">
-
-											<a href="/" class="logo">
-													<img src="/wp-content/uploads/logo-white.png" alt="" />
-											</a>
-
-									<div class="mobile-nav">
-											<i class="fa fa-bars"></i>
-									</div>
-
-									<div class="inner-nav desktop-nav">
-										<?php html5blank_nav(); ?>
-									</div>
-							</div>
-					</nav>
+		<div class="page-loader">
+				<div class="loader">Loading...</div>
+		</div>
+		<div class="page" id="top">
+				<nav class="main-nav dark transparent stick-fixed">
+					<div class="full-wrapper relative clearfix">
+						<a href="/" class="logo">
+								<img src="/wp-content/uploads/logo-white.png" alt="" />
+						</a>
+						<div class="mobile-nav">
+								<i class="fa fa-bars"></i>
+						</div>
+						<div class="inner-nav desktop-nav">
+							<?php html5blank_nav(); ?>
+						</div>
+					</div>
+				</nav>
