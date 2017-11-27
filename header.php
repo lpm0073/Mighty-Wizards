469c57e940a03380704971935ee8bbff643ed444
Lawrence McDaniel <lpm0073@gmail.com>
2017-11-27 11:22:38 -0600

    new stable state

--- header.php
+++ header.php
@@ -17,18 +17,8 @@
 
 		<link rel="icon" type="image/png" href="/wp-content/uploads/favicon.png">
 		<link rel="dns-prefetch" href="//www.google-analytics.com">
-		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.min.css">
-		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
-		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style-responsive.css">
-		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/animate.min.css">
-		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/vertical-rhythm.min.css">
-		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/owl.carousel.css">
-		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/magnific-popup.css">
-		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/rev-slider.css">
-		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/rs-plugin/css/settings.css" media="screen" />
 
 		<?php wp_head(); ?>
-
 	</head>
 	<body class="appear-animate">
 		<div class="page-loader">
