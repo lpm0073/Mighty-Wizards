ca05c4592ae2268e4483c4dacf17e583ada4e213
Lawrence McDaniel <lpm0073@gmail.com>
2017-11-27 12:27:06 -0600

    let wordpress add jquery dynamically

--- footer.php
+++ footer.php
@@ -28,7 +28,6 @@
 		ga('send', 'pageview');
 		</script>
 
-		<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery-1.11.2.min.js"></script>
 		<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery.easing.1.3.js"></script>
 		<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
 		<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/SmoothScroll.js"></script>
