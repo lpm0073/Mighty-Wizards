27f1379f9237290832f4115e58028aec5ad97bf6
Lawrence McDaniel <lpm0073@gmail.com>
2017-11-27 19:58:55 -0600

    jquery war

--- functions.php
+++ functions.php
@@ -87,36 +87,8 @@ function html5blank_nav()
 	);
 }
 
-// Load HTML5 Blank scripts (header.php)
-function html5blank_header_scripts()
-{
-    if ($GLOBALS['pagenow'] != 'wp-login.php' && !is_admin()) {
-
-    //	wp_register_script('conditionizr', get_template_directory_uri() . '/js/lib/conditionizr-4.3.0.min.js', array(), '4.3.0'); // Conditionizr
-    //    wp_enqueue_script('conditionizr'); // Enqueue it!
-
-    //    wp_register_script('modernizr', get_template_directory_uri() . '/js/lib/modernizr-2.7.1.min.js', array(), '2.7.1'); // Modernizr
-    //    wp_enqueue_script('modernizr'); // Enqueue it!
-
-    //    wp_register_script('html5blankscripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '1.0.0'); // Custom scripts
-    //    wp_enqueue_script('html5blankscripts'); // Enqueue it!
-    }
-}
 
 // Load HTML5 Blank conditional scripts
-function html5blank_conditional_scripts()
-{
-    //if (is_page('pagenamehere')) {
-    //}
-
-
-}
-
-function insert_jquery(){
-wp_enqueue_script('jquery', false, array(), false, false);
-}
-add_filter('wp_enqueue_scripts','insert_jquery',1);
-
 function add_scripts()
 {
   /**
@@ -194,11 +166,6 @@ function add_scripts()
 
 }
 
-// Load HTML5 Blank styles
-function html5blank_styles()
-{
-
-}
 
 // Register HTML5 Blank Navigation
 function register_html5_menu()
@@ -418,11 +385,8 @@ function html5blankcomments($comment, $args, $depth)
 \*------------------------------------*/
 
 // Add Actions
-add_action('wp_enqueue_scripts', 'add_scripts' ); // add all js
-add_action('init', 'html5blank_header_scripts'); // Add Custom Scripts to wp_head
-add_action('wp_print_scripts', 'html5blank_conditional_scripts'); // Add Conditional Page Scripts
+//add_action('wp_enqueue_scripts', 'add_scripts' ); // add all js
 add_action('get_header', 'enable_threaded_comments'); // Enable Threaded Comments
-add_action('wp_enqueue_scripts', 'html5blank_styles'); // Add Theme Stylesheet
 add_action('init', 'register_html5_menu'); // Add HTML5 Blank Menu
 //add_action('init', 'create_post_type_html5'); // Add our HTML5 Blank Custom Post Type
 add_action('widgets_init', 'my_remove_recent_comments_style'); // Remove inline Recent Comment Styles from wp_head()
