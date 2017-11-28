4598c665d90a5b944a7ff8caf1bd94a40ebd8333
Lawrence McDaniel <lpm0073@gmail.com>
2017-11-27 18:08:10 -0600

    blog styling

--- functions.php
+++ functions.php
@@ -186,8 +186,6 @@ function add_scripts()
 function html5blank_styles()
 {
 
-    wp_register_style('html5blank', get_template_directory_uri() . '/style.css', array(), '1.0', 'all');
-    wp_enqueue_style('html5blank'); // Enqueue it!
 }
 
 // Register HTML5 Blank Navigation
